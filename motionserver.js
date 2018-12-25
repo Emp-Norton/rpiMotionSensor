ba6f2827afd44b14c5d01221dfd3c8d64ff7260f
James Jelenko <jamesjelenko@gmail.com>
2018-12-24 21:19:50 -0800

    Adds basic logging functionality on failure to capture and send image. New notes to self to fix various things.

--- motionserver.js
+++ motionserver.js
@@ -2,7 +2,11 @@ const express = require('express');
 const {exec} = require('child_process');
 const moment = require('moment');
 const app = express();
+const fs = require('fs');
+
+
 // Decide which direction to go with this: env variables, command line arguments, or both (messy?)
+// passing arguments when using npm start? Config file?
 require('dotenv').config();
 const args = process.argv.slice(2);
 
@@ -10,7 +14,6 @@ const config = {
   port: parseInt(args[0]) || process.env.PORT,
   eaddress: args[1] || process.env.EMAIL,
 };
-
 
 app.get('/sendEmail', (req, res) => {
   const requestStart = Date.now();
@@ -24,10 +27,21 @@ In the meantime, the time the email was received will have to suffice.
 
   const sendEmail = (fileString) => {
     let exc = `echo -e "to: ${config.eaddress}\nsubject: MOTION DETECTED: ${formatTime()}\n"| (cat - && uuencode ./img/${fileString}.jpg ${fileString}.jpg) | ssmtp ${config.eaddress}`;
+    // is there really a good reason to keep the console logs here, given that the devices are headless? Methinks not.
     console.log(`Sending email: \n ${exc}`);
     exec(exc, (err, stdout, stderr) => {
-      if (err || stderr) console.log(err, stderr);
+      if (err || stderr) {
+	// need very specific time stamps for error codes, ergo using unix time instead of subpar formatTime func. Refac func or explore Moment for solution.
+	const errorString = `${Date.now()}\n${err}`
+	console.log(err, stderr);
+	// need check for existence of logs directory / permissions to create it. Look at existing log directories as storage areas.
+	fs.appendFile('./logs/email-logs.txt', errorString, (err) => {
+	  if (err) throw err;
+	  console.log('file updated');
+	})
+      }
       if (stdout) console.log(stdout);
+	// need to improve the speed, but the benchmark can be dropped for now
       const duration = (Date.now() - requestStart) / 1000;
       console.log(`Total time spent: ${duration} seconds`);
     });
