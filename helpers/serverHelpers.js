67c0690da114b57f5de5f58aeddf931a9e233cc6
James Jelenko <jamesjelenko@gmail.com>
2019-01-05 13:51:05 -0800

    Refactor serverHelper file

--- helpers/serverHelpers.js
+++ helpers/serverHelpers.js
@@ -1,22 +1,25 @@
+const {exec} = require('child_process');
+const moment = require('moment');
+const fs = require('fs');
+require('dotenv').config();
+const config = {
+    port: process.env.PORT,
+    eaddress: process.env.EMAIL
+};
+
 const sendEmail = (fileString) => {
+    const formatTime = () => { return moment(Date.now()).toString().split(' ').join(' ')};
     let exc = `echo -e "to: ${config.eaddress}\nsubject: MOTION DETECTED: ${formatTime()}\n"| (cat - && uuencode ./img/${fileString}.jpg ${fileString}.jpg) | ssmtp ${config.eaddress}`;
-    // is there really a good reason to keep the console logs here, given that the devices are headless? Methinks not.
-    console.log(`Sending email: \n ${exc}`);
     exec(exc, (err, stdout, stderr) => {
       if (err || stderr) {
-	// need very specific time stamps for error codes, ergo using unix time instead of subpar formatTime func. Refac func or explore Moment for solution.
 	const errorString = `${Date.now()}\n${err}`
 	console.log(err, stderr);
-	// need check for existence of logs directory / permissions to create it. Look at existing log directories as storage areas.
 	fs.appendFile('./logs/email-logs.txt', errorString, (err) => {
 	  if (err) throw err;
 	  console.log('file updated');
 	})
       }
       if (stdout) console.log(stdout);
-	// need to improve the speed, but the benchmark can be dropped for now
-      const duration = (Date.now() - requestStart) / 1000;
-      console.log(`Total time spent: ${duration} seconds`);
     });
   }
   const takePic = () => {
@@ -25,7 +28,6 @@ const sendEmail = (fileString) => {
     exec(photoCommand, (err, stdout, stderr) => {
       if (err || stderr) console.log(err, stderr);
       if (stdout) console.log(stdout);
-      console.log(`Photo succeeded after: ${(Date.now() - requestStart) / 1000} seconds`);
       sendEmail(fileString);
     });
   }
