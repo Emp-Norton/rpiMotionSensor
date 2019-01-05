26a345acf4f4b3d7cc588700ccf8ddafd50892c5
James Jelenko <jamesjelenko@gmail.com>
2019-01-05 13:50:47 -0800

    Convert to helper-file and remove old comments

--- motionserver.js
+++ motionserver.js
@@ -1,9 +1,10 @@
+
 const express = require('express');
 const {exec} = require('child_process');
 const moment = require('moment');
 const app = express();
 const fs = require('fs');
-
+const helpers = require('./helpers/serverHelpers');
 
 // Decide which direction to go with this: env variables, command line arguments, or both (messy?)
 // passing arguments when using npm start? Config file?
@@ -16,49 +17,15 @@ const config = {
 };
 
 app.get('/sendEmail', (req, res) => {
-  const requestStart = Date.now();
-  const formatTime = () => { return moment(Date.now()).toString().split(' ').slice(0, 4).join(' ')};
-
-/*
-For some reason, this breaks when the time string is included. #slice(0,4) and (5, ...) work, but
-renders empty string if [4] is included. It also breaks when using the momentjs #format() method.
-In the meantime, the time the email was received will have to suffice.
-*/
-
-  const sendEmail = (fileString) => {
-    let exc = `echo -e "to: ${config.eaddress}\nsubject: MOTION DETECTED: ${formatTime()}\n"| (cat - && uuencode ./img/${fileString}.jpg ${fileString}.jpg) | ssmtp ${config.eaddress}`;
-    // is there really a good reason to keep the console logs here, given that the devices are headless? Methinks not.
-    console.log(`Sending email: \n ${exc}`);
-    exec(exc, (err, stdout, stderr) => {
-      if (err || stderr) {
-	// need very specific time stamps for error codes, ergo using unix time instead of subpar formatTime func. Refac func or explore Moment for solution.
-	const errorString = `${Date.now()}\n${err}`
-	console.log(err, stderr);
-	// need check for existence of logs directory / permissions to create it. Look at existing log directories as storage areas.
-	fs.appendFile('./logs/email-logs.txt', errorString, (err) => {
-	  if (err) throw err;
-	  console.log('file updated');
-	})
-      }
-      if (stdout) console.log(stdout);
-	// need to improve the speed, but the benchmark can be dropped for now
-      const duration = (Date.now() - requestStart) / 1000;
-      console.log(`Total time spent: ${duration} seconds`);
-    });
-  }
-  const takePic = () => {
-    let fileString = `${Date.now().toString()}`;
-    let photoCommand = `raspistill -o ./img/${fileString}.jpg`;
-    exec(photoCommand, (err, stdout, stderr) => {
-      if (err || stderr) console.log(err, stderr);
-      if (stdout) console.log(stdout);
-      console.log(`Photo succeeded after: ${(Date.now() - requestStart) / 1000} seconds`);
-      sendEmail(fileString);
-    });
-  }
 
 
-  takePic();
+  const eventString = `Request received at ${moment(Date.now())}:\n`;
+  fs.appendFile('./logs/email-logs.txt', eventString, (err) => {
+    if (err) console.log(err) // there should be a high order error logging function for all of this
+    console.log('Request logged');
+  })
+
+  helpers.takePic();
   res.end();
 });
 
