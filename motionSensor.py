0eceb92ca9582f27de3b1811dc937a9ecb3a2f99
Emp-Norton <jamesjelenko@gmail.com>
2019-01-06 13:06:39 -0800

    add logging for motion detection. note about fixing errors / interrupt

--- motionSensor.py
+++ motionSensor.py
@@ -2,16 +2,24 @@ from gpiozero import MotionSensor
 import os
 import time
 import sys
-
+import datetime
 
 targetIp = str(sys.argv[1])
 targetPort = str(sys.argv[2])
 pir = MotionSensor(4)
 logFile = "/logs/motion/motion-sensor-log.txt"
 
+def writeLog(data):
+    f = open(logFile, 'w')
+    f.write(data)
+    f.close()
+
 def alert():
     cmd = "curl %s:%s/sendEmail" % (targetIp, targetPort)
     print(cmd)
+    currentTime = time.time()
+    timestamp = datetime.datetime.fromtimestamp(currentTime).strftime('%Y-%m-%d %H:%M:%S')
+    writeLog("Detection @ %s\n" % timestamp)
     os.system(cmd)
 
 def main():
@@ -22,9 +30,7 @@ def main():
 while True:
     try:
         main()
-    except Exception, e:
+    except Exception, e: # this isn't working. sigint?
 	errorString = "Error. Check log %s" % (logFile)
         print(errorString)
-	f = open(logFile, 'w')
-	f.write(e)
-	f.close()
+	writeLog("Error: \n %s \n" % errorString)
