70ee607aad8f4fd901103eaed99222a06753ad59
James Jelenko <jamesjelenko@gmail.com>
2019-03-19 22:54:54 -0700

    Refactor invocation of main function, convert some to PEP-8 standards, write TODOs for remaining debt.

--- motionSensor.py
+++ motionSensor.py
@@ -1,36 +1,45 @@
-from gpiozero import MotionSensor
 import os
 import time
 import sys
 import datetime
 
-targetIp = str(sys.argv[1])
-targetPort = str(sys.argv[2])
-pir = MotionSensor(4)
-logFile = "/logs/motion/motion-sensor-log.txt"
+from gpiozero import MotionSensor
 
-def writeLog(data):
-    f = open(logFile, 'w')
+target_ip = str(sys.argv[1])
+target_port = str(sys.argv[2])
+# TODO : Add this to the .env file since each controller will have its own GPIO configuration.
+pir = MotionSensor(4)
+log_file = "./logs/motion/motion-sensor-log.txt"
+error_log_file = "./logs/motion/motion-sensor-error-log.txt"
+
+
+def write_log(data, log_file_path):
+    # TODO: Refactor this to use 'with open' for guaranted close()
+    f = open(log_file_path, 'w')
     f.write(data)
     f.close()
 
+
 def alert():
-    cmd = "curl %s:%s/sendEmail" % (targetIp, targetPort)
-    print(cmd)
     currentTime = time.time()
     timestamp = datetime.datetime.fromtimestamp(currentTime).strftime('%Y-%m-%d %H:%M:%S')
-    writeLog("Detection @ %s\n" % timestamp)
+    # TODO: refactor this and all logger calls to accept error or historical path. 
+    # TODO : use built-in logger?
+    write_log("Detection @ %s\n" % timestamp)
+    cmd = "curl %s:%s/sendEmail" % (targetIp, targetPort)
+    print(cmd)
     os.system(cmd)
+
 
 def main():
     pir.when_motion = alert
-    pir.wait_for_motion()
+    while True:
+        pir.wait_for_motion()
 
 
-while True:
-    try:
-        main()
-    except Exception, e: # this isn't working. sigint?
-	errorString = "Error. Check log %s" % (logFile)
-        print(errorString)
-	writeLog("Error: \n %s \n" % errorString)
+try:
+    main()
+# TODO: Fix this exception for when no arguments are passed to the CLI.
+except IndexError:
+    print("Error: No arguments received.")
+    write_log("Error: \n %s \n" % errorString)
